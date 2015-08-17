1e9dbeacb0da7211a368db93a4ac590c8f243d90
khadim-raath <khadim.nu@gmail.com>
2015-08-18 03:55:57 +0500

    aws task already register hostedzone added

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,40 +8,41 @@ $(document).ready(function () {
         domain = domain.trim();
         if (domain != "") {
             $.ajax({
-                        url: base_url+"test.php",
-                        type: "POST",
-                        data: {domain: domain},
-                        success: function (data)
-                        {
-                            var result = JSON.parse(data);
-                            console.log(result);
-                            var response = result['data'];
-                            if (response) {
+                url: base_url + "test.php",
+                type: "POST",
+                data: {domain: domain},
+                success: function (data)
+                {
+                    var result = JSON.parse(data);
+                    console.log(result);
+                    var response = result['data'];
+                    if (response) {
 
-                                var html = "<div>";
-                                html += "Id: " + response['Id'];
+                        var html = "<div>";
+                        html += "Id: " + response['Id'];
+                        html += "</div>";
+
+                        var ns = response['ns'];
+                        if (ns) {
+                            for (var i = 0; i < ns.length; i++) {
+                                html += "<div>";
+                                html += "NS:" + (i + 1) + " " + ns[i];
                                 html += "</div>";
-
-                                var ns = response['ns'];
-                                if (ns) {
-                                    for (var i = 0; i < ns.length; i++) {
-                                        html += "<div>";
-                                        html += "NS:" + (i + 1) + " " + ns[i];
-                                        html += "</div>";
-                                    }
-                                    $('#result').html(html);
-                                }
-                                else {
-                                    $('#result').html(" No result found");
-                                }
                             }
-                        },
-                        error: function ()
-                        {
-                            console.log("error");
+                            $('#result').html(html);
                         }
-                    });
-                    return false;
+                        else {
+                            $('#result').html(" No result found");
+                        }
+                    }
+                },
+                error: function ()
+                {
+                    console.log("error");
+                },
+                async: false
+            });
+            return false;
         }
         else {
             alert("Please enter the domain name!");
