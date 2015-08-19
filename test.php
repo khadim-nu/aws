98e8ca4ad24f3e27b21b85e542879f78fb89bdc2
khadim-raath <khadim.nu@gmail.com>
2015-08-20 00:59:40 +0500

    re indent

--- test.php
+++ test.php
@@ -1,41 +1,41 @@
 <?php
 // documentaions http://www.orderingdisorder.com/aws/route-53/
 function get_served(){
-require_once('r53.php');
-$access_key='AKIAJCE4ZDY4NPCMCZMA';
-$secret_key='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';
-$r53 = new Route53($access_key, $secret_key);
+	require_once('r53.php');
+	$access_key='AKIAJCE4ZDY4NPCMCZMA';
+	$secret_key='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';
+	$r53 = new Route53($access_key, $secret_key);
 
 //print_r('createHostedZone<br>');
 
 //$result = $r53->createHostedZone('google.com', 'abclllllldef', 'no cooment here');
 //var_dump($result);
 
-$result=$r53->listHostedZones();
-$result=$result['HostedZone'];
+	$result=$r53->listHostedZones();
+	$result=$result['HostedZone'];
 
-$ns_arr=array();
-foreach($result as $zone){
+	$ns_arr=array();
+	foreach($result as $zone){
+		
+		if($zone['Name']==$_POST['domain']."."){
+			$id=$zone['Id'];
+			$hosted_zone=$r53->getHostedZone($id);
+			$name_servers=$hosted_zone['NameServers'];
+			$ns_arr['Id']=$zone['Id'];
+			$ns_arr['name']=$zone['Name'];
+			$ns_arr['ns']=$name_servers;
+			break;
+		}
+
+	}
+
+	if(!empty($ns_arr)){
+		echo json_encode(array("data"=>$ns_arr,"msg"=>"successfull!","status"=>true));
+	}
 	
-if($zone['Name']==$_POST['domain']."."){
-$id=$zone['Id'];
-$hosted_zone=$r53->getHostedZone($id);
-$name_servers=$hosted_zone['NameServers'];
-$ns_arr['Id']=$zone['Id'];
-$ns_arr['name']=$zone['Name'];
-$ns_arr['ns']=$name_servers;
-break;
-}
-
-}
-
-if(!empty($ns_arr)){
-echo json_encode(array("data"=>$ns_arr,"msg"=>"successfull!","status"=>true));
-}
- 
-else{
-echo json_encode(array("data"=>array($_POST['domain']),"msg"=>"No data found!","status"=>true));
-}
+	else{
+		echo json_encode(array("data"=>array($_POST['domain']),"msg"=>"No data found!","status"=>true));
+	}
 
 
 // print_r('getHostedZone<br>');
